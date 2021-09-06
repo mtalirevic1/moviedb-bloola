31dd833adc805737cf6de966f6a9771bf0d20295
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2021-09-06 15:04:58 +0200

    Thumbnail and SearchBar style improvement

---
+++ src/components/Thumbnail/Thumbnail.styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  font-size: 0;
+  @media screen and (max-width: 768px) {
+    padding: 10px;
+  }
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  max-width: 720px;
+  transition: all 0.3s;
+  object-fit: cover;
+  border-radius: 20px 20px 0 0;
+  animation: animateThumbnail 0.5s;
+
+  :hover {
+    opacity: 0.8;
+  }
+
+  @keyframes animateThumbnail {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+export const TitleBox = styled.div`
+  width: 100%;
+  max-width: 720px;
+  border-radius: 0 0 10px 10px;
+  min-height: 50px;
+  height: fit-content;
+  padding: 20px;
+  font-size: var(--fontBig);
+  background: var(--darkGrey);
+  text-align: center;
+  color: var(--white);
+`;
+
