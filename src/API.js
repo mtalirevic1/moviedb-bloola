277b502386a5a65e0ad45080b82e1611b9328198
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2021-09-06 11:34:19 +0200

    API methods for fetching movies and credits

---
+++ src/API.js
@@ -0,0 +1,25 @@
+import {
+    SEARCH_BASE_URL,
+    TOP_RATED_BASE_URL,
+    API_URL,
+    API_KEY
+} from './config';
+
+const apiSettings = {
+    fetchMovies: async (searchTerm, page) => {
+        const endpoint = searchTerm
+            ? `${SEARCH_BASE_URL}${searchTerm}&page=${page}`
+            : `${TOP_RATED_BASE_URL}&page=${page}`;
+        return await (await fetch(endpoint)).json();
+    },
+    fetchMovie: async movieId => {
+        const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+        return await (await fetch(endpoint)).json();
+    },
+    fetchCredits: async (movieId) => {
+        const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+        return await (await fetch(creditsEndpoint)).json();
+    },
+}
+
+export default apiSettings;
